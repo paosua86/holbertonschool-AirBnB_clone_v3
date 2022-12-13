7a7129245cd98491f4abe0e1994db56cc00e1ea6
paosua86 <paolasuarezb1986@gmail.com>
2022-12-13 18:02:40 -0500

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,14 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 
 # create a route /status on the object app_views
@@ -11,3 +19,16 @@ from flask import jsonify
 @app_views.route("/status")
 def status():
     return jsonify(status="OK")
+
+# Create an endpoint that retrieves the number
+# of each objects by type:
+@app_views.route("/api/v1/stats")
+def status_views():
+    data = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
