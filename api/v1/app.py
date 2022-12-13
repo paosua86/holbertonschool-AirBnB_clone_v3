01c88afb5fecf5da5c8050dfc32abd30a3f17f12
paosua86 <paolasuarezb1986@gmail.com>
2022-12-13 17:49:59 -0500

    api working

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Your first endpoint (route) will be to return the status of your API"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+# create a variable app, instance of Flask
+app = Flask(__name__)
+# register the blueprint app_views to your Flask instance app
+app.register_blueprint(app_views)
+
+# declare a method to handle @app.teardown_appcontext that calls storage.close()
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+if __name__ == "__main__":
+    """inside if __name__ == "__main__":, run your Flask server (variable app) with"""
+    if getenv('HBNB_MYSQL_HOST') and getenv('HBNB_API_PORT'):
+        app.run(host=getenv('HBNB_MYSQL_HOST'), port=getenv('HBNB_API_PORT'), threaded=True)
+    else:
+        app.run(host='0.0.0.0', port='5000', threaded=True)
